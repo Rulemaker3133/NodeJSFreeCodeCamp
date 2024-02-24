f5103a52a0b2958fe2b19394a8e4527129896123
rulemaker <127700095+Rulemaker3133@users.noreply.github.com>
2024-02-24 14:18:33 +0200

    second commit

--- app.js
+++ app.js
@@ -4,14 +4,23 @@
 // local dependency - use it only in this particular project
 // npm i <packageName> 
 
-//global dependency - use it in any ptoject
+// global dependency - use it in any ptoject
 // npm install -g <packageName> 
 // sudo npm  install -g <packageName> (mac)
+
+// dev dependency
+// npm i <packageName> -D
+// npm i <packageName> --save-dev
 
 // package.json - manifest file (stores important info about project/package)
 // manual approach (create package.json in the roort, create properties etc)
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
+
+// npm uninstall <packageName> // Uninstalling modules
+
+// npm install // Installing all the packages, reading the list from package.json
+// Useful, when there are no /node_modules or package-lock.json was lost
 
 const _ = require('lodash');
 
@@ -19,4 +28,5 @@ const items = [1, [2, [3, [4]]]];
 
 const newItems = _.flattenDeep(items); // Transforming n-D arrays in the flat array
 console.log(newItems);
+console.log('newItems');
 
